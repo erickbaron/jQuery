0e76c00d4a71d919f2be04bd574be6d8f3107dea
Erick Eduardo Baron <erickbaron123456@gmail.com>
2019-08-07 08:38:31 -0300

    Exemplo terminado

--- Exemplo01.js
+++ Exemplo01.js
@@ -2,16 +2,44 @@
 
 $(function () {
     $.nome = '';
-    $.ajax({
-        url: 'http://viacep.com.br/ws/89062270/json/',
-        method: 'get',
-        success: function (data) {
-            $localidade = data.localidade;
-            $logradouro = data.logradouro;
 
-            $("#localidade").val($localidade); //# serve para o id do html
-            $("#logradouro").val($logradouro);
-         },
-        error: function (err) { }
-    })
+    /*$('#cep').on('blur', function(){ //Quando ocorrer no #cep o evento on blur, ocoree a funcao buscarCep 
+        buscarCEP()
+    })*/
+
+    $('#cep').on("keypress", function(evt){ //através do evt identifica o cód da tecla pressionada
+        if(evt.keyCode == 13){
+            buscarCEP();
+        }
+    }) 
+    
+
+    function buscarCEP() {
+        // document.getElementById("cep").value
+        $cep = $('#cep').val().replace('-','').replace('.', '');
+        if($cep.length != 8){
+            alert('Tamanho do CEP inválido')
+            $("#cep").focus();
+            return
+        }
+
+        $.ajax({
+            url: 'http://viacep.com.br/ws/' + $cep + '/json/',
+            method: 'get',
+            success: function (data) {
+                $localidade = data.localidade;
+                $logradouro = data.logradouro;
+                $unidadeFederativa = data.uf;
+
+                $("#localidade").val($localidade); //# serve para buscar o id do html
+                $("#logradouro").val($logradouro);
+                $("#unidade-federativa").val($unidadeFederativa);
+                $("#numero").focus();
+            },
+            error: function (err) {
+                alert('CEP inválido');
+                $("#cep").focus();
+             }
+        })
+    }
 });
